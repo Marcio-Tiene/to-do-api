8a07c763db8e4a7388ed7b32dfc435f8fb690bc8
Marcio Tiene <marcio.ricardo@mettzer.com>
2022-06-04 09:25:14 -0300

    feat(usermodule): create user module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(port)
+    .then(() => {
+      console.log(`
+      
+      🔥🔥🔥 The api is running on port ${port} 🔥🔥🔥 `);
+    })
+    .catch((error) => {
+      console.error(`
+      
+      🔥🔥🔥 Te api not started because of ${error?.message}`);
+    });
 }
 bootstrap();
