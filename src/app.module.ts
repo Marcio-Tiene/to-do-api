6ea63f325ad3b211fc9e1615dfa6a7fcaf0a3b87
Marcio Tiene <marcio.ricardo@mettzer.com>
2022-06-05 10:18:22 -0300

    feat(project): create project module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
+import { ProjectModule } from './project/project.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    ProjectModule,
+  ],
 })
 export class AppModule {}
